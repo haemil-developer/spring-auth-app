941d47ecc2304545f44758a26a6229a97c2ad5ba
haemil <every.haemil@gmail.com>
2023-09-09 20:12:41 +0900

    - edit config

--- src/main/java/com/example/auth/config/SpringSecurityConfig.java
+++ src/main/java/com/example/auth/config/SpringSecurityConfig.java
@@ -1,26 +1,68 @@
 package com.example.auth.config;
 
+import com.example.auth.user.User;
+import com.example.auth.user.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SpringSecurityConfig {
-
-/*
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().requestMatchers("/signup", "/login", "/logout");
-    }
-*/
+    private final UserService userService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth.requestMatchers("/","/login","/signup").permitAll());
+        http.httpBasic(basic -> basic.disable());
+        http.csrf(csrf -> Customizer.withDefaults());
+        http.rememberMe(remember -> remember.alwaysRemember(true));
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/","/home","/signup").permitAll()
+                .requestMatchers("/note").hasRole("USER")
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/notice").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/notice").hasRole("ADMIN")
+                .anyRequest().authenticated()
+        );
+
+        http.formLogin(login -> login
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+        );
+
+        http.logout(logout -> logout
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+        );
         return http.build();
+    }
+
+    public void configure(WebSecurity web) {
+        // 정적 리소스 spring security 대상에서 제외
+        // web.ignoring().antMatchers("/images/**", "/css/**");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            User user = userService.findByUsername(username);
+            if (user == null) {
+                throw new UsernameNotFoundException(username);
+            }
+            return user;
+        };
     }
 }
